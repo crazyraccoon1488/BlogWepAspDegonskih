1f4614860c5034bac4a83ccf9434b3a49d691415
Dmitrij Temnoholmov <dnmka59@outlook.com>
2021-01-13 18:50:06 +0500

    up

---
+++ Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class User
+    {
+        [Key]
+        [DisplayName("Id")]
+        public int Id { set; get; }
+
+        [DisplayName("Ник")]
+        [Required]
+        public string Nick { set; get; }
+
+        [DisplayName("Email")]
+        [Required]
+        public string Email { set; get; }
+
+        [DisplayName("Пароль")]
+        [Required]
+        public string Password { set; get; }
+
+        [DisplayName("Дата регистрации")]
+        [Required]
+        public DateTime CurrentDate { set; get; }
+    }
+}
