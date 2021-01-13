dbb6daf72bf9c1ce3d1187270a422aa445869058
Dmitrij Temnoholmov <dnmka59@outlook.com>
2021-01-13 18:50:06 +0500

    up

---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Category
+    {
+        [Key]
+        [DisplayName("№")]
+        public int IdCategory { set; get; }
+        [DisplayName("Категория")]
+        [Required]
+        public String NameCategory { set; get; }
+    }
+}
---
+++ Models/Post.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    public class Post
+    {
+        [Key]
+        [DisplayName("IdPost")]
+        public int Id { set; get; }
+
+        [ForeignKey("User")]
+        [DisplayName("IdUser")]
+        [Required]
+        public int IdUser { set; get; }
+        public User User { set; get; }
+
+        [DisplayName("Название")]
+        [Required]
+        public string Title { set; get; }
+
+        [DisplayName("Текст")]
+        [Required]
+        public string TextComment { set; get; }
+
+        [DisplayName("Дата создания")]
+        [Required]
+        public DateTime CurrentDate { set; get; }
+
+        [ForeignKey("Category")]
+        [DisplayName("№ Категории")]
+        [Required]
+        public int IdCategory { set; get; }
+        public Category Category { set; get; }
+
+}
